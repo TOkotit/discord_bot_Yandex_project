e3afb2e36a37d2e3f3b8fad036586bc5c8de7a1b
TOkotit <tokotit32@gmail.com>
2024-04-17 21:44:39 +0500

    наконец смог запустить в одном файле функцию модерации и комманд и мне даже не написали китайцы с угрозами Надеюсь этот коммит попадёт на гитхаб

--- programm/code/command_bot.py
+++ programm/code/command_bot.py
@@ -3,6 +3,7 @@ import discord
 import asyncio
 import logging
 from discord.ext import commands
+from datetime import timedelta
 from samples import TOKEN
 
 GUILD = os.getenv('GC_TEST_GUILD')
@@ -13,39 +14,68 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+intents = discord.Intents.all()
+intents.presences = True  #
+intents.members = True  #
+intents.message_content = True  #
+
+with open('commands.txt', encoding='utf-8') as file:
+    COMMANDS = [i.strip() for i in file.readlines()]
+with open('BAN_WORDS', encoding='utf-8') as file:
+    BAN_WORDS = [i.strip() for i in file.readlines()]
+
+
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
 
 class ModerBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+
+
 
     @commands.command(aliases=['send_status'])
     async def status(self, ctx):
         for guild in bot.guilds:
             members = '\n - '.join([f"{member.name} ({member.status})" for member in guild.members])
             await ctx.send(f'Guild Members:\n - {members}')
+
     @commands.command(aliases=['check'])
-    async def check_member(self, ctx, name=None):
-        if name is None:
-            await ctx.send(f'Не указано имя пользователя')
+    async def check_member(self, ctx, member: discord.Member):
+        if member.activities:
+            activity = member.activities[0]
         else:
-            for guild in bot.guilds:
-                for member in guild.members:
-                    if member.name.lower() == name.lower():
-                        break
-            sting_n, string_t = '\n', '\t'
-            await ctx.send(f"""Guild member {name}
+            activity = None
+        sting_n, string_t = '\n', '\t'
+        await ctx.send(f"""Guild member {member.name}
 status: {member.status}\ncolor:{member.color}
-CustomActivity name: {member.activities}
+CustomActivity name: {activity}
 id: {member.id}
 roles:\n {sting_n.join([f'    {i}' for i in member.roles])}
 is on time outed: {member.timed_out_until}""")
 
-intents = discord.Intents.all()
-intents.members = True
-intents.presences = True
 
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='/', intents=intents)
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+    for i in COMMANDS:
+        if i in ctx.content.lower().split()[0]:
+            break
+    else:
+        for i in BAN_WORDS:
+            if i in ctx.content.lower():
+                await timeout_member(ctx.channel, ctx.author)
+                break
+        else:
+            await ctx.reply('я твой рот и жизнь ибал ежи')
+
+async def timeout_member(channel, member: discord.User, reason='Иди ты нахуй, выблядок'):
+    # проверяем на существование записи с id пользователя
+    await member.timeout(timedelta(minutes=2), reason=reason)
+    await channel.send(f'Участник {member.mention} был замучен.\nПричина: {reason}')
 
 
 async def main():
--- programm/code/samples.py
+++ programm/code/samples.py
@@ -1,1 +1,1 @@
-TOKEN = 'MTIyNTM5OTQ3MzAwNjcxMDgwNg.GmzjOh.fv5PU9PAJ41_MgyR0VT6-srRrBc-qY1_avFTEg'
+TOKEN = 'MTIyNTM5OTQ3MzAwNjcxMDgwNg.   G3d5Ju.IOCek-SY      _m1l5sC6axDmrRQKNPvE4CcEEwtwrc'
